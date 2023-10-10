948d5e11b5146356a1fd1123809c73f153458174
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-10-10 11:01:53 -0300

    feat: criação de dump.sql

---
+++ dump.sql
@@ -0,0 +1,23 @@
+CREATE TABLE usuarios (
+  id serial primary key,
+  nome varchar(255) not null,
+  email varchar(255) not null,
+  senha varchar(255) not null
+  );
+  
+CREATE TABLE categorias (
+  id serial primary key,
+  descricao varchar(100) not null
+  );
+  
+
+INSERT INTO categorias (descricao) VALUES
+  ('Informática'),
+  ('Celulares'),
+  ('Beleza e Perfumaria'),
+  ('Mercado'),
+  ('Livros e Papelaria'),
+  ('Brinquedos'),
+  ('Moda'),
+  ('Bebê'),
+  ('Games');
