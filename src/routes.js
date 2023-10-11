833f7be664524f630b32efb92291ae0a443d186d
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-10-11 16:30:37 -0300

    fix: ajuste em routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express();
 
 const users = require('./controllers/users');
 const { listCategories } = require('./controllers/categories');
