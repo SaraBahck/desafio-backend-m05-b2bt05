498e474390b0628d43b5886b927641f4bf8e0c74
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-10-11 19:05:01 -0300

    feat: implementação de middler de autenticação

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,13 @@ const router = express();
 const users = require('./controllers/users');
 const { listCategories } = require('./controllers/categories');
 const { userLogin } = require('./controllers/userLogin');
+const authentication = require('./middlers/authentication');
 
 router.post('/usuario', users.userRegistration);
 router.get('/categoria', listCategories);
 router.post('/login', userLogin)
+
+router.use(authentication)
+
 
 module.exports = router;
