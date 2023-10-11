c456e0bd0e31dd3207eb30fde53b94b131432dcc
mirellaor <mirellaor>
2023-10-10 23:47:48 -0300

    chore: adjust app initialization

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 require('dotenv').config();
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+const routes = require('./routes');
 
 app.use(express.json())
-
-
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
