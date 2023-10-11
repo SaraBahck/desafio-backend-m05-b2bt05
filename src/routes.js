84d6dad0998eebe4b09531f0d913f87f08f3567c
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-10-11 17:04:25 -0300

    feat: login e intermediários de validação

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,10 @@ const router = express();
 
 const users = require('./controllers/users');
 const { listCategories } = require('./controllers/categories');
+const { userLogin } = require('./controllers/userLogin');
 
 router.post('/usuario', users.userRegistration);
 router.get('/categoria', listCategories);
-
+router.post('/login', userLogin)
 
 module.exports = router;
