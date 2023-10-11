a5078a6934abd7ef970311bd887c488823c3520a
mirellaor <mirellaor>
2023-10-10 23:46:46 -0300

    feat: add new category controllers

---
+++ src/controllers/categories.js
@@ -0,0 +1,14 @@
+const knex = require('../dbConnection');
+
+async function listCategories(req, res) {
+    try {
+        const categories = await knex.select('*').from('categorias');
+        return res.status(200).json({ categories });
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+    }
+}
+
+module.exports = {
+    listCategories
+}
