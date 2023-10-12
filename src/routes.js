bc0da9f67a609740f4948000ab0af941ea6b7f6b
Gessyca Borges <gessyca.borges@gmail.com>
2023-10-12 13:33:00 -0300

    Refactor: Ajustes na rota de cadastro, inserção de funções na pasta utils

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express();
 
-const users = require('./controllers/users');
+const { userRegistration } = require('./controllers/users');
 const { listCategories } = require('./controllers/categories');
 const { userLogin } = require('./controllers/userLogin');
 const authentication = require('./middlers/authentication');
 
-router.post('/usuario', users.userRegistration);
+router.post('/usuario', userRegistration);
 router.get('/categoria', listCategories);
 
 
