8c9f6926fcda5cb45a4aab3e134eaa7f89257a03
Nataliasalles1 <natalia.salles100@gmail.com>
2023-10-13 11:07:08 -0300

    Feat: correção controllers

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -46,10 +46,11 @@ const userRegistration = async (req, res) => {
 const detailUser = async (req, res) => {
     try {
 
-        const { id, nome, email } = req.usuario
+        const { id, nome, email } = req.user
         res.status(200).json({ id, nome, email })
 
     } catch (error) {
+        console.log(error.message);
         res.status(404).json({ mensagem: `o servidor não pode encontrar o recurso solicitado.` })
     
     }
@@ -57,6 +58,6 @@ const detailUser = async (req, res) => {
 
 
 module.exports = {
-    userRegistration,
+    userRegistration, 
     detailUser
 }
