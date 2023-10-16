aef2cabe6fa507052f9ad754fd95972fd71b1948
SaraBahck <saraback.pro@gmail.com>
2023-10-16 17:14:14 -0300

    branch atualizada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const routes = require("./routes");
+const cors = require('cors')
 require('dotenv').config();
 const app = express();
 
 
-app.use(express.json())
+app.use(cors())
 
+app.use(express.json());
 
+app.use(routes);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
-})
+}); 
