f141ec666b448cb13726d083b425fe2ee91f1a11
Nataliasalles1 <natalia.salles100@gmail.com>
2023-10-16 21:31:28 -0300

    Fix: Refatoração rota detalhar User

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -24,16 +24,9 @@ const userRegistration = async (req, res) => {
 }
 
 const detailUser = async (req, res) => {
-    try {
-
-        const { id, nome, email } = req.user
-        res.status(200).json({ id, nome, email })
-
-    } catch (error) {
-        console.log(error.message);
-        res.status(404).json({ mensagem: `o servidor não pode encontrar o recurso solicitado.` })
     
-    }
+    return res.json(req.user);
+    
 }
 
 
