f45d66f970c329e2bd06d6d122719b9732c5a529
SaraBahck <saraback.pro@gmail.com>
2023-10-16 23:54:04 -0300

    merge

--- src/controllers/editUser.js
+++ src/controllers/editUser.js
@@ -1,23 +1,29 @@
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const knex = require('../dbConnection');
-
-const validateUserDataLogin = require('../utils.js/validateUserDataLogin');
-const checkEmailRegistered = require('../utils.js/checkEmailRegistered')
 
 const editUser = async (req, res) => {
     const { nome, email, senha } = req.body;
-
-    await validateUserDataLogin(nome, email, senha);
+    const id = req.user.id
+    if (!nome || !email || !senha) {
+        return res.status(400).json(({
+            mensagem: 'Por favor, preencha todos os campos!'
+        }))
+    }
 
     try {
+        if (!nome || !email || !senha) {
+            return res.status(400).json(msg('Por favor, preencha todos os campos.'))
+        }
+
+        const emailExists = await knex('usuarios').where({ email }).first()
+
+        if (id !== emailExists.id) {
+            return res.status(400).json({
+                mensagem: 'Este email já existe!'
+            });
+        }
+
         const hashPass = await bcrypt.hash(senha, 10);
-        const tokenUser = jwt.verify(token)
-        console.log("tudo certo")
-
-        if (senha) { hashPass(senha) }
-
-        await checkEmailRegistered(email)
 
         await knex('usuarios')
             .where({ id })
@@ -26,7 +32,6 @@ const editUser = async (req, res) => {
                 email,
                 senha: hashPass
             });
-
         return res.status(204).send();
 
     } catch (error) {
@@ -34,7 +39,6 @@ const editUser = async (req, res) => {
             mensagem: 'Erro interno do servidor'
         });
     }
-
 }
 
 module.exports = {
