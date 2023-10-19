b17595075f347c9f22c3f4413e7e14e2b4b2943b
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-10-19 00:20:32 -0300

    fix: criação de utilitário para localizar produto por id

--- src/controllers/detailProduct.js
+++ src/controllers/detailProduct.js
@@ -1,23 +1,15 @@
-const knex = require('../dbConnection')
+const { findProduct } = require("../utils/findProductByID")
 
 const detailProduct = async (req, res) => {
     const {id} = req.params
 
     try {
-        const product = await knex('produtos').where({
-            id
-        })
-
-        if (product.length === 0){
-            return res.status(404).json("Produto não encontrado")
-        }
+        const product = await findProduct(id)
 
         res.status(200).json(product)
 
     } catch (error) {
-        
-        res.status(404).json({ mensagem: `o servidor não pode encontrar o recurso solicitado.` })
-    
+        return res.status(error.code).json(error.message)
     }
 }
 
