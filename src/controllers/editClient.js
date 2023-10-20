c41305512a89340b51fe711a90f52389358b0454
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-10-20 18:36:11 -0300

    refactor: ajuste em validações e status code

--- src/controllers/editClient.js
+++ src/controllers/editClient.js
@@ -1,23 +1,21 @@
-const bcrypt = require('bcrypt');
 const validateClientDataRegist = require('../utils/validateClientDataRegist')
-const checkClientExists = require('../utils/checkClientExists');
 const checkEmailClientToUpdate = require('../utils/checkEmailClientToUpdate');
 const checkCpfClientToUpdate = require('../utils/checkCpfClientToUpdate');
 const updateClientIntoDatabase = require('../utils/updateClientIntoDatabase');
+const { findClientById } = require('../utils/findClientById');
 
 const editClient = async (req, res) => {
     const { id } = req.params;
     const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
 
     try {
-
-        await checkClientExists(id);
+        await findClientById(id);
         await validateClientDataRegist(nome, email, cpf);
         await checkEmailClientToUpdate(req, email);
         await checkCpfClientToUpdate(req, cpf);
         await updateClientIntoDatabase(req, nome, email, cpf, cep, rua, numero, bairro, cidade, estado);
 
-        return res.status(204).json({ mensagem: 'Cliente atualizado!!' });
+        return res.status(200).json({ mensagem: 'Cliente atualizado!' });
 
     } catch (error) {
         return res.status(error.code).json(error.message);
