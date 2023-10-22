6a2a7940d93b065eb8318585770320b33f0ce011
Nataliasalles1 <natalia.salles100@gmail.com>
2023-10-22 01:51:03 -0300

    Fix: correção do dbConnection

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,23 +1,13 @@
-// const knex = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//     ssl: { rejectUnauthorized: false }
-//   }
-// });
-
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : 'localhost',
-    user : 'postgres',
-    port : 5432,
-    password : '123456',
-    database : 'evd'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
   }
 });
+
 module.exports = knex
