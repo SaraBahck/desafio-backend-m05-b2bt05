07aa0676408a4e181fb1ad76b3c7ab2cffc54da2
mirellaor <mirellaoliveeira@hotmail.com>
2023-10-21 21:31:48 -0300

    refactor: padronizando o corpo de resposta

--- src/controllers/products/deleteProduct.js
+++ src/controllers/products/deleteProduct.js
@@ -10,15 +10,15 @@ const deleteProduct = async (req, res) => {
         const deleteProduct = await knex('produtos').where({
             id
         })
-        .del()
-        if(!deleteProduct){
+            .del()
+        if (!deleteProduct) {
             throw {
                 code: 400,
                 message: 'O produto não foi excluído'
             }
         }
-        return res.status(200).json('Produto excluido com sucesso')
-    }catch(error) {
+        return res.status(204).send();
+    } catch (error) {
         return res.status(error.code).json(error.message)
     }
 }
