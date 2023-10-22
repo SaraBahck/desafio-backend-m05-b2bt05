03fb7d7ea6ba885288c79c4d30be230ecf373368
mirellaor <mirellaoliveeira@hotmail.com>
2023-10-21 21:31:31 -0300

    refactor: padronizando o corpo de resposta

--- src/controllers/client/listClient.js
+++ src/controllers/client/listClient.js
@@ -5,7 +5,7 @@ const listClient = async (req, res) => {
 
     try {
         const clients = await knex.select('*').from('clientes');
-        return res.status(200).json({ clientes: clients });
+        return res.status(200).json(clients);
 
     } catch (error) {
 
