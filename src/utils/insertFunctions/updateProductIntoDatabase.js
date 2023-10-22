d838125d7d3d799702ccfc02d250e2d0830db380
mirellaor <mirellaoliveeira@hotmail.com>
2023-10-21 21:35:14 -0300

    fix: ajustando pra retornar o produto atualizado

--- src/utils/insertFunctions/updateProductIntoDatabase.js
+++ src/utils/insertFunctions/updateProductIntoDatabase.js
@@ -3,15 +3,17 @@ const knex = require('../../dbConnection')
 const updateProductIntoDatabase = async (id, descricao, quantidade_estoque, valor, categoria_id) => {
     const register = await knex('produtos')
         .where({ id })
-        .update({ descricao, quantidade_estoque, valor, categoria_id })
+        .update({ descricao, quantidade_estoque, valor, categoria_id }).returning('*');
     ;
-    
-    if (!register){
+
+    if (!register) {
         throw {
             code: 400,
             message: "O produto não foi atualizado"
         }
     };
+
+    return register[0];
 }
 
 module.exports = updateProductIntoDatabase
