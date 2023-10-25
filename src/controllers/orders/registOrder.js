8a0c583e4ac2fd6fef79a88d667848f6a7529b19
mirellaor <mirellaoliveeira@hotmail.com>
2023-10-25 20:12:03 -0300

    fix: arrumando a  função de registrar um pedido

--- src/controllers/orders/registOrder.js
+++ src/controllers/orders/registOrder.js
@@ -13,7 +13,7 @@ const registOrder = async (req, res) => {
     const { cliente_id, observacao, pedido_produtos } = req.body;
 
     try {
-        console.log("passo 1")
+
         await findClientById(cliente_id);
 
         for (const item of pedido_produtos) {
@@ -22,14 +22,13 @@ const registOrder = async (req, res) => {
             const product = await findProduct(produto_id);
             checkStock(product, quantidade_produto);
         }
-        console.log("passo 2")
 
         const valor_total = await calculateOrder(pedido_produtos);
 
         const order = await insertNewOrder(cliente_id, observacao, valor_total);
 
         await updateStockAndInsertOrder(pedido_produtos, order.id);
-        console.log("passo 3")
+
         await sendEmail(client)
 
         return res.status(201).json(order);
