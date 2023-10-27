b7710c345d69ef7fec377405b518af3d418ec64c
Daniela Felipe Soares <117787402+Danifeares@users.noreply.github.com>
2023-10-27 00:52:02 -0300

    fix: Update nodemailer.js

--- src/connections/nodemailer.js
+++ src/connections/nodemailer.js
@@ -4,11 +4,13 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    secureProtocol: "TLSv1_2_method"
 })
 
 module.exports = transporter
